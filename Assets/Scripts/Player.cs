640cc1f03158ddcd125de1bae2a00040ba2b365f
Zack Rock <zacharyrock@hotmail.com>
2016-02-11 00:27:26 -0800

    Init push.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour {
+
+	bool clickStarted = false;
+
+	List<Vector3> mousePositions;
+
+	public GameObject projectilePrefab;
+
+	void Start () {
+		mousePositions = new List<Vector3>();
+	}
+	
+	void Update () {
+		if(Input.GetButtonDown("Fire1")) {
+			clickStarted = true;
+			mousePositions.Clear();
+		}
+		if(clickStarted) {
+			mousePositions.Add(Input.mousePosition);
+		}
+		if(Input.GetButtonUp("Fire1")) {
+			clickStarted = false;
+			FireProjectile();
+		}
+	}
+
+	public void FireProjectile() {
+		GameObject projectile = (GameObject)Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+		projectile.GetComponent<Projectile>().SetPositions(mousePositions);
+	}
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Projectile : MonoBehaviour {
+
+	public float speed;
+
+	public GameObject particle;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	public void SetPositions(List<Vector3> positions) {
+		StartCoroutine(Move(positions));
+	}
+
+	IEnumerator Move(List<Vector3> positions) {
+		for(int i = 0; i < positions.Count; i++) {
+			
+//			RaycastHit2D hit = Physics2D.Raycast((Vector2)Camera.main.ScreenToWorldPoint(positions[i]), Vector2.zero); 
+//			print(Camera.main.ScreenToWorldPoint(positions[i]));
+			Vector3 pos =  Camera.main.ScreenToWorldPoint(positions[i]);
+			pos.z = 0f;
+			transform.position = pos;
+
+
+			yield return new WaitForSeconds(speed);
+		}
+		Destroy(gameObject, speed);
+		Instantiate(particle, transform.position, Quaternion.identity);
+	}
+}
