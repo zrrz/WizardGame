e8b8ec815dd09ac23388ded9dce2b9bdb37f1ad1
Wollem <tom.matney@aol.com>
2016-03-13 05:35:24 -0400

    Wizard and clouds

--- Assets/Scripts/CloudGenerator.cs
+++ Assets/Scripts/CloudGenerator.cs
@@ -72,20 +72,20 @@ public class CloudGenerator : MonoBehaviour {
 			Debug.LogError("No spawn points", this);
 			return;
 		}
-		Vector3 pos = spawnPoints[Random.Range(0, spawnPoints.Length+1)].position;
+		Vector3 pos = spawnPoints[Random.Range(0, spawnPoints.Length)].position;
 		pos.y += Random.Range(-heightVariation, heightVariation);
 
 		if(cloudPrefabs.Length == 0) {
 			Debug.LogError("No cloud prefabs", this);
 			return;
 		}
-		GameObject cloudPrefab = cloudPrefabs[Random.Range(0, cloudPrefabs.Length + 1)];
+		GameObject cloudPrefab = cloudPrefabs[Random.Range(0, cloudPrefabs.Length)];
 
 		GameObject cloudObj = (GameObject)Instantiate(cloudPrefab, pos, Quaternion.identity);
 		Cloud cloud = new Cloud();
 		cloud.cloudObj = cloudObj;
 		cloud.speed = Random.Range(minSpeed, maxSpeed);
-		cloud.direction = pos.x < 0f ? Cloud.Direction.Right : Cloud.Direction.Right;
+		cloud.direction = pos.x < 0f ? Cloud.Direction.Right : Cloud.Direction.Left;
 		clouds.Add(cloud);
 	}
 
