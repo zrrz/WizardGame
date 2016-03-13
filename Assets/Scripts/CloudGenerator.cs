dc88a6f352ec8dfb6809b8aec6e9799c0f82ded2
Zack Rock <zacharyrock@hotmail.com>
2016-03-13 00:15:38 -0800

    CloudGenerator. Killed some warnings.

---
+++ Assets/Scripts/CloudGenerator.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CloudGenerator : MonoBehaviour {
+
+	public GameObject cloudPrefab;
+
+	public Transform[] spawnPoints;
+
+	public bool seedClouds = true;
+
+	List<Cloud> clouds;
+
+	[Range(0f,5f), SerializeField]
+	float frequencyMin;
+	[Range(0f,5f), SerializeField]
+	float frequencyMax;
+
+	[Range(0f,5f), SerializeField]
+	float minSpeed;
+	[Range(0f,5f), SerializeField]
+	float maxSpeed;
+
+	[Range(0f,5f), SerializeField]
+	float minSize;
+	[Range(0f,5f), SerializeField]
+	float maxSize;
+
+	[Range(0f,5f), SerializeField]
+	float heightVariation;
+
+	class Cloud {
+		public GameObject cloudObj;
+		public float speed;
+		public enum Direction {Left, Right}
+		public Direction direction;
+
+		public float CalcDirection() {
+			if(direction == Direction.Left)
+				return -1f;
+			else
+				return 1f;
+		}
+	}
+
+	IEnumerator Start () {
+		clouds = new List<Cloud>();
+		if(seedClouds)
+			SeedClouds();
+
+		while(true) {
+			yield return new WaitForSeconds(Random.Range(frequencyMin, frequencyMax));
+			SpawnCloud();
+		}
+	}
+		
+	void Update () {
+		for(int i = 0; i < clouds.Count; i++) {
+			float direction = clouds[i].CalcDirection() * clouds[i].speed * Time.deltaTime;
+			clouds[i].cloudObj.transform.Translate(direction, 0f, 0f);
+			if(clouds[i].cloudObj.GetComponent<Renderer>().isVisible == false) {
+				Destroy(clouds[i].cloudObj);
+				clouds.RemoveAt(i);
+				i--;
+			}
+		}
+	}
+
+	void SpawnCloud() {
+		if(spawnPoints.Length == 0) {
+			Debug.LogError("No spawn points", this);
+			return;
+		}
+		Vector3 pos = spawnPoints[Random.Range(0, spawnPoints.Length)].position;
+		pos.y += Random.Range(-heightVariation, heightVariation);
+
+		if(cloudPrefab == null) {
+			Debug.LogError("Cloud prefab is empty", this);
+			return;
+		}
+		GameObject cloudObj = (GameObject)Instantiate(cloudPrefab, pos, Quaternion.identity);
+		Cloud cloud = new Cloud();
+		cloud.cloudObj = cloudObj;
+		cloud.speed = Random.Range(minSpeed, maxSpeed);
+		cloud.direction = (Cloud.Direction)Random.Range(0, 1);
+		clouds.Add(cloud);
+	}
+
+	void SeedClouds() {
+		float seedTime = 15f;
+
+		int numClouds = Mathf.RoundToInt(seedTime/((frequencyMin+frequencyMax)/2f));
+
+		for(int i = 0; i < numClouds; i++) {
+			SpawnCloud();
+			clouds[i].cloudObj.transform.Translate(clouds[i].CalcDirection() * clouds[i].speed * seedTime, 0f, 0f);
+		}
+	}
+}
